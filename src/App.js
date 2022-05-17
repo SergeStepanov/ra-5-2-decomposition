96fa40c6713edd688123a67d9c5294cc23eed054
Sergey Stepanov <ser1653285@yandex.ru>
2022-05-17 08:32:02 +0300

    end

--- src/App.js
+++ src/App.js
@@ -3,28 +3,108 @@ import List from './components/List';
 import ListItem from './components/ListItem';
 import { nanoid } from 'nanoid';
 import Widget from './components/Widget';
+import Link from './components/Link';
+import Input from './components/Input';
+import Button from './components/Button';
 
 import data from './data';
-const { newsWidget, currencyWidget } = data;
-
+const { newsWidget, currencyWidget, inputTop, program, visited, broadcast } =
+  data;
 function App() {
   return (
     <div className='container'>
-      <Widget title={<a href="#0">Сейчас в сми</a>}>
+      <Widget title={<Link href='#0'>Сейчас в сми</Link>}>
         <List className='row' items={newsWidget}>
           {(items) =>
             items.map((item) => (
               <ListItem key={nanoid()} className='col-12'>
-                <a href={item.link}>
-                  {item.logo}
-                  {item.title}
-                </a>
+                <Link href={item.link}>{`${item.logo} ${item.title}`}</Link>
               </ListItem>
             ))
           }
         </List>
+        <List className='row' items={currencyWidget}>
+          {(items) =>
+            items.map((item) => (
+              <ListItem
+                className='col-3'
+                key={nanoid()}>{`${item.currency} ${item.amount}`}</ListItem>
+            ))
+          }
+        </List>
       </Widget>
-      {/* {console.log(data)} */}
+      <Widget>
+        <span>
+          <Link href='#0'>Яндекс</Link>
+        </span>
+
+        <List className='row' items={inputTop}>
+          {(items) =>
+            items.map((item) => (
+              <ListItem key={nanoid()} className='col'>
+                <Link href='#0'>{item}</Link>
+              </ListItem>
+            ))
+          }
+        </List>
+        <form>
+          <Input type='text' name='name' style={{ width: '70%' }} required />
+          <Button className='btn'>Найти</Button>
+          <label className='w-100' htmlFor='name'>
+            Найдется все.
+          </label>
+        </form>
+      </Widget>
+      <Widget>
+        <p className='banner'>Баннер рекламы</p>
+      </Widget>
+      <div className='row'>
+        <Widget className='col-4' title={<Link href='#0'>Погода</Link>}>
+          <p>Какой-то прогноз погоды</p>
+        </Widget>
+
+        <Widget className='col-4' title={<Link href='#0'>Карта Германии</Link>}>
+          {<Link href='#0'>Расписание</Link>}
+        </Widget>
+
+        <Widget className='col-4' title={<Link href='#0'>Эфир</Link>}>
+          <List items={broadcast}>
+            {(items) =>
+              items.map((item) => (
+                <ListItem key={nanoid()}>
+                  <Link
+                    href={item.link}>{`${item.title} ${item.channel}`}</Link>
+                </ListItem>
+              ))
+            }
+          </List>
+        </Widget>
+
+        <Widget className='col-4' title={<Link href='#0'>Посещаемое</Link>}>
+          <List className='row' items={visited}>
+            {(items) =>
+              items.map((item) => (
+                <ListItem key={nanoid()}>
+                  <Link href='#0'>{item}</Link>
+                </ListItem>
+              ))
+            }
+          </List>
+        </Widget>
+
+        <Widget className='col-4' title={<Link href='#0'>Телепрограмма</Link>}>
+          <List items={program}>
+            {(items) =>
+              items.map((item) => (
+                <ListItem key={nanoid()}>
+                  <Link
+                    href={item.link}>{`${item.title} ${item.channel}`}</Link>
+                </ListItem>
+              ))
+            }
+          </List>
+        </Widget>
+      </div>
     </div>
   );
 }
--- src/components/Widget.js
+++ src/components/Widget.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
-export default function Widget({children, title}) {
+/**
+ * Компонент Widget группирует компоненты и элементы
+ */
+
+export default function Widget({children, title, ...props}) {
   return (
-    <section>
-      <h4>{title}</h4>
+    <section {...props}>
+      {title && <h4>{title}</h4>}
       {children}
     </section>
   )
